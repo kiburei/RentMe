69ac0351b414b1d80690c3e2ede5306c8e136877
kiburei <akiburei@gmail.com>
2018-01-24 11:48:48 +0300

    config index page

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Index from './src/main/index';
+
+export default class App extends React.Component {
+  render() {
+    return <Index />;
+  }
+}
---
+++ src/main/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <Text>Rent Me</Text>
+    );
+  }
+}
